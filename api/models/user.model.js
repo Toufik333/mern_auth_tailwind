f19632f9db6c5294d2930260aa68802fcd63df8d
Toufik333 <75950762+Toufik333@users.noreply.github.com>
2024-09-03 01:01:24 +0600

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    pass: {
+        type: String,
+        required: true,
+    },
+    }, {timeseries: true}); //each user will have two extra info. The time they were created and the time they were last updated
+
+const User = mongoose.model('User', userSchema); // User must me uppercase and singular
